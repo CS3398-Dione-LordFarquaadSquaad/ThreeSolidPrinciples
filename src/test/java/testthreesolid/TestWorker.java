9ae18941906d18986266fe36e96cb8c1070b9e4a
raa92 <42945188+raa92@users.noreply.github.com>
2018-10-23 19:19:00 -0500

    Update testWorker.java

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -1,32 +1,15 @@
 package assignment5;
-
-import org.junit.jupiter.api.BeforeAll;
-
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
 public class TestWorker {
+  private BaseWorker w = new BaseWorker();
 
-   private BaseWorker w = new BaseWorker();
-	
-   @Test
-   @DisplayName("No Name Test")
-   public void testWorkerWithNoName()
-   {
-      assertEquals(w.getName(),"");
-      assertEquals(w.work(),"I'm working already!");
-   }
-
-   @Test
-   @DisplayName("With Name Test")
-   public void testWorkerWithName()
-   {
-      w.setName("Corvus Glaive");
-      assertEquals(w.getName(),"Corvus Glaive");
-      assertEquals(w.work(),"Corvus Glaive is working very hard!");
-   }
-
+  @Test
+  @DisplayName("Work Method Test")
+  public void testWorkMethod()
+  {
+    assertEquals(w.work(),"I'm working already!",”Mismatch between test text and method text”);
+  }
 }
