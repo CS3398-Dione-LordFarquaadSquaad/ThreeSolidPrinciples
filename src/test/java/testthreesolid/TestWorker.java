dd646bb31d68232b04f18aed536b56c20d90f3a4
raa92 <42945188+raa92@users.noreply.github.com>
2018-10-25 23:14:09 -0500

    Add passing test

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -6,10 +6,18 @@ import org.junit.jupiter.api.Test;
 public class TestWorker {
   BaseWorker w = new BaseWorker("Bob", 12345);
 
+  // Given test
   @Test
   @DisplayName("Work Method Test")
-  public void testWorkMethod()
-  {
+  public void testWorkMethod() {
     assertEquals(w.work(),"I'm working already!","Mismatch between test text and method text");
+  }
+  
+  // Passing test
+  @Test
+  @DisplayName("Worker Name Test")
+  public void testSetNameMethod() {
+    w.setName("Trevor");
+    assertEquals(w.getName(), "Trevor");
   }
 }
