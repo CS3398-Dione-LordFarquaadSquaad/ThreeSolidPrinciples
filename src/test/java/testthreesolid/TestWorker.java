b058ea5c1ab86a5c07ebd78d57ab17419cc8b868
Laura D <laura.doan@yahoo.com>
2018-10-27 20:56:08 -0500

    Added 1 passing and one failing test

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 
 public class TestWorker {
   BaseWorker w = new BaseWorker("Bob", 12345);
+    BaseWorker w2 = new BaseWorker("Steve", 12345);
 
   @Test
   @DisplayName("Work Method Test")
   public void testWorkMethod()
   {
     assertEquals(w.work(),"I'm working already!","Mismatch between test text and method text");
+    assertNull(w.aId, "aID filled");
+    assertEquals(w.aId, w2.aId, "aID's should be identical");
   }
 }
