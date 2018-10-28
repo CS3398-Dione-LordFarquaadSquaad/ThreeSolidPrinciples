615acfb05bda99bf0055d32c2b728e9d05c1973c
rarnecke <rca31@txstate.edu>
2018-10-28 00:16:17 -0500

    Tests added
    One pass test, one fail test added to TestWorker class

--- src/test/java/testthreesolid/TestWorker.java
+++ src/test/java/testthreesolid/TestWorker.java
@@ -12,4 +12,19 @@ public class TestWorker {
   {
     assertEquals(w.work(),"I'm working already!","Mismatch between test text and method text");
   }
+
+  @Test
+  @DisplayName("R-Arnecke Worker Pass Test")
+  public void newtestRAWorkerPass()
+  {
+  	assertTrue(w.ID == 12345);
+  }
+
+  @Test
+  @DisplayName("R-Arnecke Worker Fail Test")
+  public void newtestRAWorkerFail()
+  {
+  	BaseWorker x = w;
+  	assertNotSame(x, w);
+  }
 }
